6e0aa37f6058f1643c9b224ee48b8c5299c0f659
Maksim Grishenko <endtimes104@gmail.com>
2018-10-30 21:02:27 +0200

    update route index

--- routes/routes.php
+++ routes/routes.php
@@ -1,18 +1,13 @@
 <?php 
 
 Flight::route('/', function(){
-	Flight::render('index', array('title' => 'Flight'));
-});
-Flight::route('/temp', function(){
-    $arr = array(
-        ['text' => 'game'],
-        ['text2' => 'game2'],
-    );
-    new Controllers\TempController(uniqid(),$arr);
-});
-Flight::route('/temp/read', function(){
-    $arr = Controllers\TempController::openContent('5bd87e8f383c9');
-
-    $data = json_decode($arr,true);
-    var_dump($data);
+	Flight::render('index', array(
+	    'title' => settings()['site']['title'],
+      'description' => settings()['site']['description'],
+      'keywords' => settings()['site']['keywords'],
+      'server_ip' => settings()['server']['ip'],
+      'links' => settings()['links'],
+      'widget' => settings()['widget'],
+      'prodcuts' => products(),
+  ));
 });
