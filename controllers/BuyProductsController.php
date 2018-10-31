3273079ae9aebe98c89b3be306a6186c49b2e79f
Maksim Grishenko <endtimes104@gmail.com>
2018-10-31 18:44:22 +0200

    create BuyProductController

---
+++ controllers/BuyProductsController.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace Controllers;
+
+use Controllers\TempController as Temp;
+use Flight;
+
+class BuyProductsController
+{
+
+    public static function order($name,$id_privilege)
+    {
+        $product = products()[$id_privilege];
+        $kassa = settings();
+        $id = uniqid();
+        self::isEmpty($name,$id_privilege);
+
+        new Temp($id,[
+            'id' => $id,
+            'user' => $name,
+            'privilage' => $product['pex'],
+            'price' => $product['price']
+        ]);
+
+
+        $hash = md5($kassa['shop_id'].":".$product['price'].":".$kassa['key'].":".$id);
+        Flight::json([
+            'status' => 'success',
+            'message' => 'http://www.free-kassa.ru/merchant/cash.php?m='.$kassa['shop_id'].'&oa='.$product['price'].'&s='.$hash.'&o='.$id
+        ]);
+
+    }
+
+    private static function isEmpty($name,$id_privilege)
+    {
+        if (empty($name))
+        {
+            Flight::json([
+                'status' => 'error',
+                'message' => 'Введите свой ник',
+            ]);
+            exit();
+        }
+        if (empty($id_privilege))
+        {
+            Flight::json([
+                'status' => 'error',
+                'message' => 'Такой привилегии нету',
+            ]);
+            exit();
+        }
+    }
+
+}
