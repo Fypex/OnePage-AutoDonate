651a629801e1303408b0709754787e7f35d3b06f
Maksim Grishenko <endtimes104@gmail.com>
2018-10-31 19:49:55 +0200

    added RconController

---
+++ controllers/RconController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Controllers;
+
+use thedudeguy\Rcon;
+
+class RconController
+{
+
+    public static function command($command)
+    {
+        $settings = settings()['rcon'];
+
+        $host = $settings['ip'];
+        $port = $settings['port'];
+        $password = $settings['password'];
+        $timeout = 3;
+
+
+        $rcon = new Rcon($host, $port, $password, $timeout);
+
+        if ($rcon->connect())
+        {
+            $rcon->send_command($command);
+        }
+    }
+}
