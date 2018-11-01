6a72ce16afb23e6c09a5ac423840f6213b7e946e
Maksim Grishenko <endtimes104@gmail.com>
2018-11-01 02:06:53 +0200

    added error and success views

---
+++ views/error.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title><?= $title ?></title>
+    <meta name="description" content="<?= $description ?>">
+    <meta name="keywords" content="<?= $keywords ?>">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="icon" type="image/png" href="/assets/img/favicon.png">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-v4-grid-only@1.0.0/dist/bootstrap-grid.min.css">
+    <link rel="stylesheet" href="/css/style.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Montserrat:400,600,700&subset=cyrillic">
+</head>
+<body style="background: #F44336;min-height: 100vh;overflow-y: hidden">
+    <h1 style="color: white;padding-top: 40vh">Оплата не удалась!</h1>
+<script>
+    setTimeout("window.location.href = '/'",3000);
+</script>
+</body>
+</html>
